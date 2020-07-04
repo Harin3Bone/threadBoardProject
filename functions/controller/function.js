469d47d4a080c05eed51fb0570f24c7bf039a565
Harin.Thananam <harin.thananam@gmail.com>
2020-07-04 14:26:06 +0700

    Import Crypto
    For use encryption & decryption

--- functions/controller/function.js
+++ functions/controller/function.js
@@ -1,3 +1,9 @@
+//! Import Crypto
+const crypto = require('crypto');
+const algorithm = 'aes-256-cbc';
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
 //# USER
 //? Email Checker -> Must have @ and .com
 function validateEmail(email) {
@@ -43,6 +49,26 @@ function getOnlyTime(dateTime) {
     return threadTime;
 }
 
+//# Encryption & Decryption
+function encrypt(text) {
+    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return {
+        iv: iv.toString('hex'),
+        encryptedData: encrypted.toString('hex')
+    };
+}
+
+function decrypt(text) {
+    let iv = Buffer.from(text.iv, 'hex');
+    let encryptedText = Buffer.from(text.encryptedData, 'hex');
+    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
+
 //! Export
 module.exports = {
     validateEmail,
@@ -50,5 +76,7 @@ module.exports = {
     getUsernameFromId,
     getDateTime,
     getOnlyDate,
-    getOnlyTime
+    getOnlyTime,
+    encrypt,
+    decrypt
 }
